8f1e316176b12888483048ecde1c7225541d2632
Saidul Mondal <mondalsaidul3232@gmail.com>
2020-11-12 22:57:18 +0530

    data-add-router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const Product = require('../models/Product');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//add product
+router.post('/', async(req, res) => {
+  const { product_name, price, category } = req.body;
+          try {
+              const product = new Product({ product_name, price, category })
+              await product.save()
+              res.status(201).send("succesfully added");
+            
+          } catch(e) {
+            res.status(400).send(e)
+     }
+})
 
 module.exports = router;
