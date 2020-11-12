9bac227cd550f1bb6d8920b14ed644eb48635ddd
Saidul Mondal <mondalsaidul3232@gmail.com>
2020-11-12 23:41:55 +0530

    export-csv-router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const csv = require('csv-express');
+const mongoose = require('mongoose');
+const Product  = mongoose.model('Products');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+ router.get('/', function(req, res, next) {
+    Product.find({}, function(err, products) {
+        if (err)
+          res.send(err);
+
+        res.render('index', { title: 'Nodejs MongoDB export to CSV', products: products });
+    });
+ });
+
+ router.get('/exporttocsv', function(req, res, next) {
+    const filename = "products.csv";
+    const dataArray;
+    Product.find().lean().exec({}, function(err, products) {
+        if (err) res.send(err);
+        
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/csv');
+        res.setHeader("Content-Disposition", 'attachment; filename='+filename);
+        res.csv(products, true);
+    });
+ });  
+
+
 
 module.exports = router;
