182483a0801cc4b9d85f549a2810ca8a96fe97c1
Saidul Mondal <mondalsaidul3232@gmail.com>
2020-11-12 22:47:00 +0530

    product-scheema

---
+++ models/Product.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+
+  product_name: { type: String, Required:  'Product name cannot be left blank.' },
+
+  price:    { type: String,     Required:  'Product price cannot be left blank.'},
+
+  category: { type: String ,    Required:  'Product category cannot be left blank'}
+
+});
+
+module.exports = mongoose.model('Products', productSchema);
